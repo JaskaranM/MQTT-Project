b958f9c258943d708e114b22a79de40866844158
sidsgav <159062182+sidsgav@users.noreply.github.com>
2024-02-20 21:40:30 +0000

    Update mqttProject.py
    added a new branch to show progress of project rather than updating main as it shows no progress (more marks). - or can just update main one up to you

--- mqttProject.py
+++ mqttProject.py
@@ -18,13 +18,24 @@ def on_connect(client, userdata, flags, rc):
     
     if rc == 0:
         print("Connected to broker")
-        client.subscribe("status/location/bikeID")  # Subscribe to the desired topic(s)
+        client.subscribe("owntracks/GS/GS")  # Subscribe to the desired topic(s) - Subscribe to the OwnTracks topic where location updates are published
     else:
         print("Connection failed. RC: " +str(rc))
 
 def on_message(client, userdata, msg):
     
     print(msg.topic+" "+str(msg.payload))
+
+def handle_example_message(payload):
+    # Parse the message payload and execute actions based on its content
+    
+    data = json.loads(payload)
+    action = data.get("action") # stop bike/ continue after stopping/ stop using bike....
+    if action == "do_something":
+        # Execute the appropriate action
+        print("Doing something...") # print message to client updating of the status of bike...
+        
+        # use of variables such as location/ latitiude etc will be useful 
 
 # set up MQTT client
 
@@ -41,7 +52,7 @@ client.loop_start()
 client.loop_stop()
 client.disconnect() #maybe use a try/except to handle this or allow it to loop forever until declared
 
-# potential functions:
+# potential functions: // may have to be classes now for OOP marks 
 
 def userReg():
 
@@ -51,6 +62,10 @@ def locTracking():
 
     pass
 
+def update_location(locData):
+    
+    pass
+
 # potential data types (include in documentation)
 
 bikeData = 0 # bike ID
@@ -58,3 +73,6 @@ bikeData = 0 # bike ID
 userData = 1 # can use a database here
 
 locData = 2 #including latitude, longitude, timestamp
+
+# also consider using a database - maybe SQLite....
+# keep it relatively simple and just let it store and update user info, location... 
