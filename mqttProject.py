6e3dc748d8cb95d43b8ba1965259eba22971b6c4
sidsgav <159062182+sidsgav@users.noreply.github.com>
2024-03-10 20:29:00 +0000

    Add files via upload
    Made some changes/ added some example functions and classes to establish connections between database and frontend etc... / and some overall documentation to explain code and show next steps

--- mqttProject.py
+++ mqttProject.py
@@ -2,6 +2,8 @@
 
 import paho.mqtt.client as mqtt
 import time
+import json
+import sqlite3
 
 # MQTT broker information
 
@@ -11,6 +13,25 @@ broker_port = 1883
 # Create MQTT client instance
 
 client = mqtt.Client("bike_ID")
+
+# SQLite database initialization
+
+db_test_file = "bike_data.db"
+connectTo = sqlite3.connect(db_test_file) # name of Aman's file...
+c = connectTo.cursor()
+
+# Create table if it doesn't already exist
+
+c.execute('''CREATE TABLE IF NOT EXISTS bike_location
+             (id INTEGER PRIMARY KEY AUTOINCREMENT,
+              bike_id TEXT,
+              latitude REAL,
+              longitude REAL,
+              timestamp INTEGER)''')
+
+# Commit changes and close the database connection
+
+connectTo.commit()
 
 # define a callback function on_message to handle messages received on subscribed topics.
 
@@ -34,8 +55,22 @@ def handle_example_message(payload):
     if action == "do_something":
         # Execute the appropriate action
         print("Doing something...") # print message to client updating of the status of bike...
+
+            # Publish the message to the specified topic
+        publish.single("owntracks/GS/GS", json.dumps(action), hostname="test.mosquitto.org", port=1883)
+        print("Message published:", action)
+        
+        time.sleep(10)  # Wait for 10 seconds before publishing the next message # can be changed
+
+# Call the function to publish example messages
+publish_example_messages()
         
         # use of variables such as location/ latitiude etc will be useful 
+
+    # Insert data into SQLite database
+    c.execute('''INSERT INTO bike_location (bike_id, latitude, longitude, timestamp) 
+                 VALUES (?, ?, ?, ?)''', (bike_id, latitude, longitude, timestamp)) # whatever the fields are...
+    connectTo.commit()  # Commit changes to the database
 
 # set up MQTT client
 
@@ -76,3 +111,34 @@ locData = 2 #including latitude, longitude, timestamp
 
 # also consider using a database - maybe SQLite....
 # keep it relatively simple and just let it store and update user info, location... 
+
+# need to establish connection between tkinter GUI, subscriber script for MQTT, and database
+
+# implement the GUI components - necessary buttons etc...(basically just finish frontend)
+
+# Add MQTT Subscriber Functionality - incorporate functionality in Jas' Tkinter application to subscribe to 
+# MQTT messages. we can use the paho.mqtt.client library within the Tkinter application when merged/ declared
+# to establish a connection to the MQTT broker and receive messages.
+
+# Update GUI with MQTT Data - implement logic to update the Tkinter GUI with the data received from MQTT 
+# messages. For example, if we receive location data for bikes, we should update relevant labels or display 
+# the data in a list or table within your Tkinter application and update the database as needs be.
+
+# Integrate SQLite database interaction - use the sqlite3 library to interact with the SQLite database.
+
+# Update GUI with database data - somehow...
+
+# example functions could include:
+
+class BikeTrackerApp:
+    def __init__(self, master):
+        self.master = master
+        master.title("Bike Tracker")
+
+
+    def query_database(self):
+        # Query SQLite database for relevant data
+        self.c.execute("SELECT * FROM bike_location")
+        data = self.c.fetchall()
+        # Process and update GUI with database data
+
