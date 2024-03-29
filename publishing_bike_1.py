0d9fa0d4510bc01394cbd7b9285873967ebeec4f
bobbstr <adejobbyb@gmail.com>
2024-03-29 18:21:22 +0000

    made bike publishing file for publishing messages

--- publishing_bike_1.py
+++ publishing_bike_1.py
@@ -5,11 +5,9 @@ import paho.mqtt.client as paho
 def print_msg(client, userdata, message):
     print("%s : %s" % (message.topic, message.payload))
 
-# Define SSL settings
 ssl_settings = ssl.create_default_context()
 ssl_settings.check_hostname = False
 
-# Put in your cluster credentials and hostname
 auth = {'username': "Bike1", 'password': "MQTTBike1"}
 subscribe.callback(print_msg, "mqtt/location/bike1", hostname="da26e2370918479aba885bc54297a1d6.s1.eu.hivemq.cloud",
                    port=8883, auth=auth, tls=ssl_settings, protocol=paho.MQTTv311)
